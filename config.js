197b7df8c10026a5d7720b370cb214d35191948e
hai0bd <dosonhai2004@gmail.com>
2021-10-24 19:44:14 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hee luu!',
+    introDesc: `Tớ có điều muốn hỏi, cậu phải trả lời thật lòng nhá🤧`,
+    btnIntro: 'oke',
+    title: 'Cậu có yêu tớ không?😙',
+    desc: '*Chú ý: Nếu như cậu không trả lời hoặc thoát ra thì nghĩa là cậu ngầm đồng ý nhưng ngại ngùng không nói ra nha!! 🤭',
+    btnYes: 'Yêu ơi là yêu <33',
+    btnNo: 'Cậu mơ à :333',
+    question: 'Nói cảm nhận của cậu về tớ đi <3',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Vì cậu đẹp trai và rất tốt bụng hihi :3 ',
+    mess: 'Tớ biết mà hí hí hí 🥰',
+    messDesc: 'Còn muốn nói gì nữa thì inbox cho Hải nè.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://facebook.com/profile.php?id=100040287383083' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
